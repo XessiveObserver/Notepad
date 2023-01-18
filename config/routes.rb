dda3179a642c7790f2051e5869dabf348b4a5874
XessiveObserver <xessiveobserver@gmail.com>
2023-01-18 15:59:26 +0300

    devise auth added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'static_pages/index'
   get 'static_pages/about'
   get 'static_pages/contact'
   
   resources :notes
   
-
-    
-  root "static_pages#index"
+  root "static_pages#index"    
 end
