20961c1e7c272c1fdc72936c1827de84692bba3f
XessiveObserver <xessiveobserver@gmail.com>
2023-02-02 08:47:54 +0300

    reversed notes listings

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,7 @@
 class NotesController < ApplicationController
+  before_action :specific_note, only: [:update, :edit, :destroy]
   def index
-    @notes = Note.all
+    @notes = Note.all.reverse
   end
 
   def show
@@ -8,12 +9,13 @@ class NotesController < ApplicationController
   end
 
   def new
-    @note = Note.new
+    # @note = Note.new
+    @note = current_user.notes.build
   end
 
   def create
-    @note = Note.new(note_params)
-    @note.user = current_user
+    # @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
     
     if @note.save
       redirect_to @note
@@ -41,6 +43,11 @@ class NotesController < ApplicationController
     @note.destroy
 
     redirect_to notes_path, status: :see_other
+  end
+
+  def specific_note
+    @note = current_user.notes.find_by(user_id: params[:id])
+    redirect_to notes_path, notice: "Not allowed to make changes to this Note" if @notice.nil?
   end
 
   private
